f838e96b8c0edc0130f133f2f80103b32d498492
krish bishwas <you@example.com>
2026-07-16 16:41:02 +0545

    Add user login functionality; implement login endpoint and validation

--- services/auth.js
+++ services/auth.js
@@ -1,5 +1,7 @@
-import { create } from './user.js';
+import { create, find } from './user.js';
 import { generateAccessToken } from '../config/jwt.js';// Service function for registering a new user and generating an access token
+import {UnauthorizedError} from '../errors/unauthorized.js'; // Importing the UnauthorizedError class for handling authentication errors
+import {compare } from 'bcrypt'; // Importing the compare function from bcrypt for password comparison
 
 export const register = async (data) => {
     const user = await create(data);
@@ -7,9 +9,18 @@ export const register = async (data) => {
 }
 
 export const login = async (data) => {
-    const user = await find({ email: data.email });
-    if(!await compare(data.password, user.password)) {
-        throw new UnauthorizedError('Invalid credentials');
+    let user;
+    try {
+        user = await find({ email: data.email });
+    } catch (error) {
+        if (error.name === 'NotFoundError') {
+            throw new UnauthorizedError('Invalid credentials.');
+        }
+        throw error;
+    }
+
+    if (!(await compare(data.password, user.password))) {
+        throw new UnauthorizedError('Invalid credentials.');
     }
     return generateAccessToken({ userId: user._id });
 }
